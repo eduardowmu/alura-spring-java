c2f07849ea77f3fd84a43b99226f6e1a0ddbe70c
Eduardo Watanabe <eduardo.watanabe@vilt.local>
2026-01-25 10:18:11 -0300

    Step Inicial

--- codetickets/src/main/java/br/com/alura/codetickets/ImportJobConfig.java
+++ codetickets/src/main/java/br/com/alura/codetickets/ImportJobConfig.java
@@ -5,16 +5,33 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class ImportJobConfig {
+    @Autowired
+    private PlatformTransactionManager transactionManager;
     @Bean//gerenciado pelo spring
     public Job job(Step initialStep, JobRepository jobRepository) {
         return new JobBuilder("tickets-generation", jobRepository)
                 .start(initialStep)
                 .incrementer(new RunIdIncrementer())
+                .build();
+    }
+
+    @Bean
+    public Step initialStep(ItemReader<Import> reader, ItemWriter<Import> writer,
+                            JobRepository jobRepository) {
+        return new StepBuilder("initial-step", jobRepository)
+                .<Import, Import>chunk(200, this.transactionManager)
+                .reader(reader)
+                .writer(writer)
                 .build();
     }
 }
