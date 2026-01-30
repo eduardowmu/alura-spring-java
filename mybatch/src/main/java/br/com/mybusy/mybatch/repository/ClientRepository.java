37bf5c7d22ba751103d52a21341fe105ad1f9b2a
Eduardo Watanabe <eduardo.watanabe@vilt.local>
2026-01-30 18:46:32 -0300

    projeto inicial

--- mybatch/src/main/java/br/com/mybusy/mybatch/repository/ClientRepository.java
+++ mybatch/src/main/java/br/com/mybusy/mybatch/repository/ClientRepository.java
@@ -1,2 +1,9 @@
-package br.com.mybusy.mybatch.repository;public interface ClientRepository {
+package br.com.mybusy.mybatch.repository;
+
+import br.com.mybusy.mybatch.model.MyClient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<MyClient, Long> {
 }
---
+++ mybatch/src/test/java/br/com/mybusy/mybatch/MybatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mybusy.mybatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
