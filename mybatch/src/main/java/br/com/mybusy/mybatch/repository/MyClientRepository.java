36c45d5528a0d1083a7fb02d5492256765c6a20f
eduardowmu <30440158+eduardowmu@users.noreply.github.com>
2026-02-02 21:48:19 -0300

    Delete mybatch directory

--- mybatch/src/main/java/br/com/mybusy/mybatch/repository/MyClientRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.mybusy.mybatch.repository;
-
-import br.com.mybusy.mybatch.model.MyClient;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MyClientRepository extends JpaRepository<MyClient, Long> {
-}
